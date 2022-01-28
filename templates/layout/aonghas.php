1efb83afa14235508fde6c497b11cb005b1e330a
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2022-01-28 14:59:13 +0800

    Aonghas: Removed the leftover 'customise AdminLTE' feature

--- templates/layout/aonghas.php
+++ templates/layout/aonghas.php
@@ -173,11 +173,6 @@ $cakeDescription = 'Logejo Internal';
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
